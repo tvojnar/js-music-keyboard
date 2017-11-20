a14767d2e14943aa0bfb62eca45734ac45b59759
Tamira Vojnar <tamiravojnar@gmail.com>
2017-11-20 15:04:30 -0800

    methodized the click part of the function to further dry up code

--- noise.js
+++ noise.js
@@ -1,7 +1,7 @@
 // TODO: get this method to work so I can DRY my code
 // QUESTION: how can I do string interpolation in a jQuery object to be able to select the right element by id?
 const playNote = (note) => {
-  // concatinate to make the correct id name 
+  // concatinate to make the correct id name
   let toPlay = note + 'Audio'
   // play the note
   $(`#${toPlay}`)[0].play();
@@ -10,34 +10,10 @@ const playNote = (note) => {
 } // end playNote
 
 $(document).ready( function() {
-  $('.c').on('click', function() {
-    playNote('c');
-  }); // .on
-
-  $('.d').on('click', function() {
-    playNote('d');
-  }); // .on
-
-  $('.e').on('click', function() {
-    playNote('e');
-  }); // .on
-
-  $('.f').on('click', function() {
-    playNote('f');
-  }); // .on
-
-  $('.g').on('click', function() {
-    playNote('g');
-  }); // .on
-
-  $('.a').on('click', function() {
-    playNote('a');
-  }); // .on
-
-  $('.b').on('click', function() {
-    playNote('b');
-  }); // .on
-
+  $('.instrument').on('click', 'button', function(){
+    let key = $(this).html()
+    playNote(key);
+  }) // .on
 
   $('body').keypress(function(e) {
     let note = String.fromCharCode(e.which)

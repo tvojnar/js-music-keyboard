eb6a35fb050e7abc7fe5b096b401a28534f1e238
Tamira Vojnar <tamiravojnar@gmail.com>
2017-11-20 14:48:52 -0800

    DRY up code completly to use the function

--- noise.js
+++ noise.js
@@ -1,9 +1,8 @@
 // TODO: get this method to work so I can DRY my code
 // QUESTION: how can I do string interpolation in a jQuery object to be able to select the right element by id?
 const playNote = (note) => {
+  // concatinate to make the correct id name 
   let toPlay = note + 'Audio'
-  // let play = $(`#${toPlay}`)[0];
-  // console.log(play);
   // play the note
   $(`#${toPlay}`)[0].play();
   // reset to the start so it can play again without refreshing the page
@@ -43,33 +42,19 @@ $(document).ready( function() {
   $('body').keypress(function(e) {
     let note = String.fromCharCode(e.which)
     if (note === 'c') {
-      $('#cAudio')[0].play();
-      // reset to the start so
-      $('#cAudio')[0].currentTime = 0;
+      playNote('c');
     } else if (note === 'd') {
-      $('#dAudio')[0].play();
-      // reset to the start so
-      $('#dAudio')[0].currentTime = 0;
+      playNote('d');
     } else if (note === 'e') {
-      $('#eAudio')[0].play();
-      // reset to the start so
-      $('#eAudio')[0].currentTime = 0;
+      playNote('e');
     } else if (note === 'f') {
-      $('#fAudio')[0].play();
-      // reset to the start so
-      $('#fAudio')[0].currentTime = 0;
+      playNote('f');
     } else if (note === 'g') {
-      $('#gAudio')[0].play();
-      // reset to the start so
-      $('#gAudio')[0].currentTime = 0;
+      playNote('g');
     } else if (note === 'a') {
-      $('#aAudio')[0].play();
-      // reset to the start so
-      $('#aAudio')[0].currentTime = 0;
+      playNote('a');
     } else if (note === 'b') {
-      $('#bAudio')[0].play();
-      // reset to the start so
-      $('#bAudio')[0].currentTime = 0;
+      playNote('b');
     } // if else if
   }) // keypress
 }); // .ready
